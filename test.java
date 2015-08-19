a9340233a89e7692469016dc53497b3d8b0604b0
pavel <pavel@localhost.localdomain>
2015-08-19 17:15:37 +0300

    3sf

--- test.java
+++ test.java
@@ -4,6 +4,6 @@ class HelloWorld
    { 
 public static void main(String args[ ]) 
       { 
-System.out.println( Welcome to the world of Java!!!! ); 
+System.out.println(!!!! Welcome to the world of Java!!!! ); 
        } 
    } 
