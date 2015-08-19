0aae72dc55ec6e2a5a54a027850ba1a1cf450513
pavel <pavel@localhost.localdomain>
2015-08-19 12:22:01 +0300

    first

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.io.*; 
+// This is a simple program called HelloWorld.java 
+class HelloWorld 
+   { 
+public static void main(String args[ ]) 
+      { 
+System.out.println( Welcome to the world of Java ); 
+       } 
+   } 
